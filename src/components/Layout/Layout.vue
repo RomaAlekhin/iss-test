03c71435d4fb8776a2ad4de1ae23fe68f0e3d2fb
RomaAlekhin <raipon@yandex.ru>
2024-03-20 21:15:10 +0100

    feat: add dark theme

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark, useToggle } from "@vueuse/core";
+import { Sun, MoonStar } from "lucide-vue-next";
+import Button from "@/components/ui/button/Button.vue";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
 <template>
-  <div class="w-screen h-screen">
-    <h1 class="text-center text-3xl font-bold underline mb-4">ISS test</h1>
+  <div class="w-screen h-screen" :class="isDark ? 'dark' : ''">
+    <div class="mb-4 p-2 flex justify-center">
+      <h1 class="flex-1 text-center text-3xl font-bold underline">ISS test</h1>
+
+      <Button
+        class="rounded-full"
+        variant="outline"
+        size="icon"
+        @click="toggleDark()"
+      >
+        <Sun v-if="isDark" class="w-5 h-5" />
+        <MoonStar v-else class="w-5 h-5" />
+      </Button>
+    </div>
 
     <slot></slot>
   </div>
